eaabe40bab2cad2e5f0905a523bbb07cd4163d50
Rayen Messaoudi <20291421+rayenmessaoudi@users.noreply.github.com>
2026-01-24 18:16:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ new Vue({
           name: "Mekanın Sahibi",
           artist: "Norm Ender",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
-          source: "track0.mp3",
+          source: "audio/track0.mp3",
           url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
           favorited: false
         },
